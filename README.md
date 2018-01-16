fe0de180791230d0ee0fb2f24d36404460e629ae
Justin Fuhrmeister-Clarke <justin@fuhrmeister-clarke.com>
2018-01-16 15:32:54 +1100

    Signed-off-by: Justin Fuhrmeister-Clarke <justin@fuhrmeister-clarke.com>
    Updated for plugin.audio.vision

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# plugin.audio.example
-Online Streaming Audio Example Add-on for Kodi
+# plugin.audio.vision
+Add-on to stream audio from Vision Christian Radio and Vision180 Radio
+
+Currently in BETA, if there are issues please let me know
+
--- default.py
+++ default.py
@@ -7,41 +7,40 @@ import urlparse
 import xbmcaddon
 import xbmcgui
 import xbmcplugin
-# http://docs.python-requests.org/en/latest/
-import requests
-# http://www.crummy.com/software/BeautifulSoup/bs4/doc/
-from bs4 import BeautifulSoup
 
 def build_url(query):
     base_url = sys.argv[0]
     return base_url + '?' + urllib.urlencode(query)
     
-def get_page(url):
-    # download the source HTML for the page using requests
-    # and parse the page using BeautifulSoup
-    return BeautifulSoup(requests.get(url).text, 'html.parser')
+def build_song_list():
+    '''
+    Vision      128k    http://tx.sharp-stream.com/icecast.php?i=ucbau.aac
+    Vision      64k     http://tx.sharp-stream.com/icecast.php?i=ucbau.mp3
+    Vision180   128k    http://tx.sharp-stream.com/http_live.php?i=vision180.aac
+    Vision180   64k     http://tx.sharp-stream.com/http_live.php?i=vision180.mp3
+    '''
+    song_list = []
+    #vision
+    li = xbmcgui.ListItem(label='Vision Christian Radio 128k',thumbnailImage='https://vision.org.au/wp-content/themes/visionmedia/images/vision-logo.png')
+    li.setProperty('IsPlayable', 'true')
+    url = build_url({'mode': 'stream', 'url': 'http://tx.sharp-stream.com/icecast.php?i=ucbau.aac', 'title': 'Vision Christian Radio 128k'})
+    song_list.append((url, li, False))
+    li = xbmcgui.ListItem(label='Vision Christian Radio 64k',thumbnailImage='https://vision.org.au/wp-content/themes/visionmedia/images/vision-logo.png')
+    li.setProperty('IsPlayable', 'true')
+    url = build_url({'mode': 'stream', 'url': 'http://tx.sharp-stream.com/icecast.php?i=ucbau.mp3', 'title': 'Vision Christian Radio 64k'})
+    song_list.append((url, li, False))
     
-def parse_page(page):
-    songs = {}
-    index = 1
-    # the sample below is specific for the page we are scraping
-    # you will need to view the source of the page(s) you are
-    # planning to scrape to find the content you want to display
-    # this will return all the <a> elements on the page:
-    # <a href="some_url">some_text</a>
-    for item in page.find_all('a'):
-        # the item contains a link to an album cover
-        if item['href'].find('.jpg') > 1:
-            # format the url for the album cover to include the site url and url encode any spaces
-            album_cover = '{0}{1}'.format(sample_page, item['href'].replace(' ', '%20'))
-        # the item contains a link to a song containing '.mp3'
-        if item['href'].find('.mp3') > 1:
-            # update dictionary with the album cover url, song filename, and song url
-            songs.update({index: {'album_cover': album_cover, 'title': item['href'], 'url': '{0}{1}'.format(sample_page, item['href'])}})
-            index += 1
-    return songs
+    #vision180
+    li = xbmcgui.ListItem(label='Vision180 Radio 128k',thumbnailImage='http://vision180.org.au/wp-content/themes/vision180/images/vision-logo.png')
+    li.setProperty('IsPlayable', 'true')
+    url = build_url({'mode': 'stream', 'url': 'http://tx.sharp-stream.com/http_live.php?i=vision180.aac', 'title': 'Vision180 Radio 128k'})
+    song_list.append((url, li, False))
+    li = xbmcgui.ListItem(label='Vision180 Radio 64k',thumbnailImage='http://vision180.org.au/wp-content/themes/vision180/images/vision-logo.png')
+    li.setProperty('IsPlayable', 'true')
+    url = build_url({'mode': 'stream', 'url': 'http://tx.sharp-stream.com/http_live.php?i=vision180.mp3', 'title': 'Vision180 Radio 64k'})
+    song_list.append((url, li, False))
     
-def build_song_list(songs):
+    '''
     song_list = []
     # iterate over the contents of the dictionary songs to build the list
     for song in songs:
@@ -56,6 +55,7 @@ def build_song_list(songs):
         url = build_url({'mode': 'stream', 'url': songs[song]['url'], 'title': songs[song]['title']})
         # add the current list item to a list
         song_list.append((url, li, False))
+    '''
     # add list to Kodi per Martijn
     # http://forum.kodi.tv/showthread.php?tid=209948&pid=2094170#pid2094170
     xbmcplugin.addDirectoryItems(addon_handle, song_list, len(song_list))
@@ -75,18 +75,14 @@ def main():
     
     # initial launch of add-on
     if mode is None:
-        # get the HTML for http://www.theaudiodb.com/testfiles/
-        page = get_page(sample_page)
-        # get the content needed from the page
-        content = parse_page(page)
+
         # display the list of songs in Kodi
-        build_song_list(content)
+        build_song_list()
     # a song from the list has been selected
     elif mode[0] == 'stream':
         # pass the url of the song to play_song
         play_song(args['url'][0])
     
 if __name__ == '__main__':
-    sample_page = 'http://www.theaudiodb.com/testfiles/'
     addon_handle = int(sys.argv[1])
     main()
